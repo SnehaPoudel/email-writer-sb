962520a0b46290c9f686c7c1c6a9c08cb1dc705f
Sneha <sneha.poudel@deerwalk.edu.np>
2025-07-08 12:50:02 +0545

    Generate controller, service and a email request; Build the prompt.

---
+++ src/main/java/com/email/email_writer/app/EmailgeneratorService.java
@@ -0,0 +1,28 @@
+package com.email.email_writer.app;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailgeneratorService {
+
+    public String generateEmailReply(EmailRequest emailRequest){
+        // Build the prompt
+        String prompt = buildPrompt(emailRequest);
+
+        // Craft a request
+
+
+
+        // Do request and get response
+        // Return response
+    }
+    private String buildPrompt(EmailRequest emailRequest) {
+        StringBuilder prompt = new StringBuilder();
+        prompt.append("Generate a professional email reply for the following email content. Please don't generate a subject line. ");
+        if(emailRequest.getTone() != null && !emailRequest.getTone().isEmpty()){
+            prompt.append("Use a ").append(emailRequest.getTone()).append(" tone.");
+        }
+        prompt.append("\nOriginal email: \n").append(emailRequest.getEmailcontent());
+        return prompt.toString();
+    }
+}
